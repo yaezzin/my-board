00ae1e4f9548d994be7ca3fd554798e7841ab45f
yejin <yaezzin@gmail.com>
2022-03-06 02:27:49 +0900

    feat: MemberRepository 테스트 추가

---
+++ src/test/java/zero/zeroapp/repository/member/MemberRepositoryTest.java
@@ -0,0 +1,181 @@
+package zero.zeroapp.repository.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import zero.zeroapp.entity.member.Member;
+import zero.zeroapp.entity.member.Role;
+import zero.zeroapp.exception.MemberNotFoundException;
+import zero.zeroapp.repository.role.RoleRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+    @Autowired RoleRepository roleRepository;
+    @PersistenceContext EntityManager em;
+
+    @Test
+    void createAndReadTest() {
+        // given : 멤버 생성
+        Member member = createMember();
+
+        // when : 리포지토리에 저장
+        memberRepository.save(member);
+        clear();
+
+        // then : 조회 성능 검사
+        Member foundMember = memberRepository.findById(member.getId()).orElseThrow(MemberNotFoundException::new);
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+    }
+
+    @Test
+    void memberDateTest() {
+        // BaseTimeEntity 클래스의 필드들이 자동으로 추가되어 생성되는지 테스트
+        // given
+        Member member = createMember();
+
+        // when
+        memberRepository.save(member);
+        clear();
+
+        // then
+        Member foundMember = memberRepository.findById(member.getId()).orElseThrow(MemberNotFoundException::new);
+        assertThat(foundMember.getCreatedDate()).isNotNull();
+        assertThat(foundMember.getModifiedDate()).isNotNull();
+        assertThat(foundMember.getCreatedDate()).isEqualTo(foundMember.getModifiedDate());
+    }
+
+    @Test
+    void updateTest() {
+        // given
+        String updatedNickname = "updated";
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when
+        Member foundMember = memberRepository.findById(member.getId()).orElseThrow(MemberNotFoundException::new);
+        foundMember.updateNickname(updatedNickname);
+        clear();
+
+        // then
+        Member updatedMember = memberRepository.findById(member.getId()).orElseThrow(MemberNotFoundException::new);
+        assertThat(updatedMember.getNickname()).isEqualTo(updatedNickname);
+    }
+
+    @Test
+    void deleteTest() {
+        // given
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when
+        memberRepository.delete(member);
+        clear();
+
+        // then
+        assertThatThrownBy(() -> memberRepository.findById(member.getId()).orElseThrow(MemberNotFoundException::new))
+                .isInstanceOf(MemberNotFoundException.class);
+    }
+
+    @Test
+    void findByEmail() {
+        // given
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when
+        Member foundMember = memberRepository.findByEmail(member.getEmail()).orElseThrow(MemberNotFoundException::new);
+
+        // then
+        assertThat(foundMember.getEmail()).isEqualTo(member.getEmail());
+    }
+
+    @Test
+    void findByNickname() {
+        // given
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when
+        Member foundMember = memberRepository.findByNickname(member.getNickname()).orElseThrow(MemberNotFoundException::new);
+
+        // then
+        assertThat(foundMember.getNickname()).isEqualTo(member.getNickname());
+    }
+
+    @Test
+    void existsByEmail() {
+        // given
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when, then
+        assertThat(memberRepository.existsByEmail(member.getEmail())).isTrue();
+        assertThat(memberRepository.existsByEmail(member.getEmail() + "test")).isFalse();
+    }
+
+    //중복성 테스트
+    @Test
+    void uniqueEmailTest() {
+        // given
+        Member member = memberRepository.save(createMember("email1", "password1", "username1", "nickname1"));
+        clear();
+
+        // when, then
+        assertThatThrownBy(() -> memberRepository.save(createMember(member.getEmail(), "password2", "username2", "nickname2")))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    void uniqueNicknameTest() {
+        // given
+        Member member = memberRepository.save(createMember("email1", "password1", "username1", "nickname1"));
+        clear();
+
+        // when, then
+        assertThatThrownBy(() -> memberRepository.save(createMember("email2", "password2", "username2", member.getNickname())))
+                .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    void existsByNickname() {
+        // given
+        Member member = memberRepository.save(createMember());
+        clear();
+
+        // when, then
+        assertThat(memberRepository.existsByNickname(member.getNickname())).isTrue();
+        assertThat(memberRepository.existsByNickname(member.getNickname() + "test")).isFalse();
+    }
+
+    // 테스트 용 메서드
+    private void clear() {
+        em.flush(); //쿼리 즉시 수행
+        em.clear(); //캐시를 비움
+    }
+
+    private Member createMemberWithRoles(List<Role> roles) {
+        return new Member("email", "password", "username", "nickname", roles);
+    }
+
+    private Member createMember(String email, String password, String username, String nickname) {
+        return new Member(email, password, username, nickname, emptyList());
+    }
+
+    private Member createMember() {
+        return new Member("email", "password", "username", "nickname", emptyList());
+    }
+
+}
