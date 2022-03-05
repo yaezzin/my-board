7f4e6172c4bfc6b211232370cca19e911bc6a572
yejin <yaezzin@gmail.com>
2022-03-06 01:27:39 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/zero/zeroapp/ZeroappApplication.java
@@ -0,0 +1,13 @@
+package zero.zeroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zero/zeroapp/ZeroappApplicationTests.java
@@ -0,0 +1,13 @@
+package zero.zeroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
