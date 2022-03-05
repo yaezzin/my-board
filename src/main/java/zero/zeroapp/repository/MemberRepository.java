b5d799846576f8634e2fa9485ddd539cf05739c4
yejin <yaezzin@gmail.com>
2022-03-06 01:56:57 +0900

    feat: 멤버 리포지토리 추가

---
+++ src/main/java/zero/zeroapp/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package zero.zeroapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zero.zeroapp.entity.member.Member;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+
+}
