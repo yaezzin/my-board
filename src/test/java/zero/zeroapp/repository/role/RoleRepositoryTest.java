897006d7fc8d129592652301d91331845552b58a
yejin <yaezzin@gmail.com>
2022-03-06 02:32:04 +0900

    feat: RoleRepository 테스트 추가

---
+++ src/test/java/zero/zeroapp/repository/role/RoleRepositoryTest.java
@@ -0,0 +1,70 @@
+package zero.zeroapp.repository.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import zero.zeroapp.entity.member.Role;
+import zero.zeroapp.entity.member.RoleType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+class RoleRepositoryTest {
+
+    @Autowired RoleRepository roleRepository;
+    @PersistenceContext EntityManager em;
+
+    @Test
+    void createAndReadTest() { // 1
+        // given
+        Role role = createRole();
+
+        // when
+        roleRepository.save(role);
+        clear();
+
+        // then
+        Role foundRole = roleRepository.findById(role.getId()).orElseThrow(RoleNotFoundException::new);
+        assertThat(foundRole.getId()).isEqualTo(role.getId());
+    }
+
+    @Test
+    void deleteTest() { // 2
+        // given
+        Role role = roleRepository.save(createRole());
+        clear();
+
+        // when
+        roleRepository.delete(role);
+
+        // then
+        assertThatThrownBy(() -> roleRepository.findById(role.getId()).orElseThrow(RoleNotFoundException::new))
+                .isInstanceOf(RoleNotFoundException.class);
+    }
+
+    @Test
+    void uniqueRoleTypeTest() { // 3
+        // given
+        roleRepository.save(createRole());
+        clear();
+
+        // when, then
+        assertThatThrownBy(() -> roleRepository.save(createRole()))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+    }
+
+    private Role createRole() {
+        return new Role(RoleType.USER);
+    }
+
+    private void clear() {
+        em.flush();
+        em.clear();
+    }
+}
