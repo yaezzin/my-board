35127b9a4fe21a32a8c4ac2133abf4b92557902f
yejin <yaezzin@gmail.com>
2022-03-07 00:36:31 +0900

    feat: 로그인 기능 dto

---
+++ src/main/java/zero/zeroapp/dto/SignInResponse.java
@@ -0,0 +1,11 @@
+package zero.zeroapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SignInResponse {
+    private String accessToken;
+    private String refreshToken;
+}
