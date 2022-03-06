89749475bf52390036c8bab077392dfadd61084e
yejin <yaezzin@gmail.com>
2022-03-06 23:31:10 +0900

    feat: Security Config 설계

---
+++ src/main/java/zero/zeroapp/config/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package zero.zeroapp.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() // http basic 인증방식 비활성화
+                .formLogin().disable() // form login 비활성화
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 유지 x
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll(); // 모든 url에 대해서 접근 허용
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // PasswordEncoder의 구현체로 DelegatingPasswordEncoder를 사용
+    }
+
+}
